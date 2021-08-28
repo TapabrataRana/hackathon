75996d643c5871fa0b542b7173e70809b9007edb
TapabrataRana <60056140+TapabrataRana@users.noreply.github.com>
2021-08-28 22:51:28 +0530

    Add files via upload

---
+++ src/Components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './About.css'
+
+export default function About() {
+    return (
+        <div>
+        <div class="about-section">
+    <div class="inner-container">
+      <h1>About Us</h1>
+      <p class="text">
+        My name is Tapabrata Rana.I am the owner and designer of this company.Products sold here are at reasonable price.
+      </p>
+    </div>
+  </div>
+    </div>
+    )
+}
---
+++ src/Components/body.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+
+export default function body() {
+    return (
+        <div className="beta">
+      <div className="banner">
+      <div className="banner__content">
+        <div className="container">
+          <div className="banner__text">
+            <h3>MOBILE STORE</h3>
+            <p>
+              Providing the best smartphones at reasonable price with additional warranty period
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+   </div>
+    )
+}
